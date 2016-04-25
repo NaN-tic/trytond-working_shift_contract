e5bdd36ede6bf783fc78d8286c0ff2f81f0b0004
Bernat Brunet Torruella <bernat@nan-tic.com>
2016-04-25 11:33:08 +0200

    Contrlo duplicate invoice lines and working shit not duplicate it's related working shift and invoice line

--- invoice.py
+++ invoice.py
@@ -13,3 +13,12 @@ class InvoiceLine:
         'Working Shifts', readonly=True)
     interventions = fields.One2Many('working_shift.intervention',
         'customer_invoice_line', 'Interventions', readonly=True)
+
+    @classmethod
+    def copy(cls, lines, default=None):
+        if default is None:
+            default = {}
+        default = default.copy()
+        default['working_shifts'] = None
+        default['interventions'] = None
+        return super(InvoiceLine, cls).copy(lines, default=default)
