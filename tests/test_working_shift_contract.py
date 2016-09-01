99c4d700fa64beae9b82d8bc4d3bddc3c54dbac3
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-09-01 17:05:42 +0200

    Migrate test to version 4.0

--- tests/test_working_shift_contract.py
+++ tests/test_working_shift_contract.py
@@ -3,23 +3,14 @@
 import doctest
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('working_shift_contract')
-
-    def test0005views(self):
-        'Test views'
-        test_view('working_shift_contract')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'working_shift_contract'
 
 
 def suite():
@@ -27,5 +18,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_working_shift_contract.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
